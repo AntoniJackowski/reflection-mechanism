b8cbd5f6234ab7dfdf1b773d315faa7010d18644
AntoniJackowski <antekjac2@gmail.com>
2026-06-01 08:51:14 +0200

    Create basic structure for controller, add fx:id for controlls and containers in GUI

--- src/main/java/com/example/reflectionmechanism/HelloController.java
+++ src/main/java/com/example/reflectionmechanism/HelloController.java
@@ -1,14 +1,40 @@
 package com.example.reflectionmechanism;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField classNameInput;
+
+    @FXML
+    private GridPane propertiesGrid;
+
+    @FXML
+    private TextArea consoleOutput;
+
+    // Reference to currently edited object
+    private Object currentObject;
+
+    @FXML
+    public void initialize() {
+        // Set default value
+        classNameInput.setText("com.example.reflectionmechanism.Song");
+    }
+
+    @FXML
+    protected void onCreateObjectClick() {
+        String className = classNameInput.getText().trim();
+
+        // Reflection logic
+    }
+
+    @FXML
+    protected void onSaveChangesClick() {
+
+        // Save logic
     }
 }
